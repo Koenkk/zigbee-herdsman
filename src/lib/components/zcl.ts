168966f9ee5596d4fdae60215fdb0b027234996e
Koen Kanters <koenkanters94@gmail.com>
2019-07-29 20:50:17 +0200

    Add more tests.

--- src/lib/components/zcl.ts
+++ src/lib/components/zcl.ts
@@ -1,15 +1,39 @@
 import {ZclFrame} from '../../zcl';
 var zclPacket = require('../../zcl-packet');
+const assert = require('assert');
 
 module.exports = {
     frame: zclPacket.frame,
     parse: function (buf: Buffer, clusterID: number, callback) {
         const cb = (error, result) => {
-            console.log('\n\n\n', result, '\n\n\n');
-
             const frame = ZclFrame.fromBuffer(clusterID, buf);
-
-            console.log(frame, "\n\n\n\n");
+            const converted = {
+                frameCntl: {
+                    frameType: frame.Header.frameControl.frameType,
+                    manufSpec: frame.Header.frameControl.manufacturerSpecific === true ? 1 : 0,
+                    direction: frame.Header.frameControl.direction,
+                    disDefaultRsp: frame.Header.frameControl.disableDefaultResponse === true ? 1 : 0,
+                },
+                manufCode: frame.Header.manufacturerCode == null ? 0 : frame.Header.manufacturerCode,
+                seqNum: frame.Header.transactionSequenceNumber,
+                cmdId: result.cmdId,
+                payload: frame.Payload,
+            };
+            console.log('assert');
+            try {
+                assert.deepEqual(result, converted, "FAILD");
+                console.log('SUCCESSS==========')
+                console.log(buf);
+                console.log('ENDDD========');
+            } catch (e) {
+                console.log('======')
+                console.log(result);
+                console.log('======')
+                console.log(converted);
+                console.log('+++++++++++++')
+                console.log(e);
+                console.log('0000000000');
+            }
 
             callback(error, result);
         };
