a12e4d91211b6f6e014b567f4c07d71481d25aa0
Onne Gorter <onne@onnlucky.com>
2019-08-31 21:19:46 +0200

    export event

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,17 @@ import {Controller} from "./controller";
 import {Events} from "./controller/events";
 export {Controller, Events};
 
+import {
+    MessagePayload,
+    DeviceAnnouncePayload,
+    DeviceJoinedPayload,
+    DeviceInterviewPayload,
+    DeviceLeavePayload
+} from "./controller/events";
+
+export type MessageEvent = MessagePayload;
+export type AnnounceEvent = DeviceAnnouncePayload;
+export type JoinedEvent = DeviceJoinedPayload;
+export type InterviewEvent = DeviceInterviewPayload;
+export type LeaveEvent = DeviceLeavePayload;
+
