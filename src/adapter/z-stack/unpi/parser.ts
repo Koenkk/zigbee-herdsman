128798f69afe51ac33b125a1d5b89b8465aea30c
Koen Kanters <koenkanters94@gmail.com>
2019-09-14 08:25:40 +0200

    Correctly start when retrieving incomplete message. https://github.com/Koenkk/zigbee2mqtt/issues/1960

--- src/adapter/z-stack/unpi/parser.ts
+++ src/adapter/z-stack/unpi/parser.ts
@@ -15,6 +15,16 @@ class Parser extends stream.Transform {
 
     public _transform(chunk: Buffer, _: string, cb: Function): void {
         debug(`<-- [${chunk.toJSON().data}]`);
+
+        if (this.buffer.length === 0 && chunk[0] !== SOF) {
+            const index = chunk.indexOf(SOF);
+            if (index != -1) {
+                chunk = chunk.slice(index);
+            } else {
+                chunk = Buffer.alloc(0);
+            }
+        }
+
         this.buffer = Buffer.concat([this.buffer, chunk]);
         this.parseNext();
         cb();
