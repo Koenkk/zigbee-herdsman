f60281987c5bf7e2ca6fe6377dc6b8a2880df187
Koen Kanters <koenkanters94@gmail.com>
2019-10-17 19:35:44 +0200

    Another attempt.

--- src/utils/realpathSync.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-import fs from 'fs';
-
-/* Only used for mocking purposes */
-function realpathSync(path: string): string {
-    return fs.realpathSync(path);
-}
-
-export default realpathSync;
