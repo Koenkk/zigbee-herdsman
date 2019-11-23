6a3db95d10078b9db44cf0154880f6507e996314
Koen Kanters <koenkanters94@gmail.com>
2019-11-23 18:53:18 +0100

    Update touchlink.

--- src/controller/touchlink.ts
+++ src/controller/touchlink.ts
@@ -2,7 +2,6 @@ import {Adapter} from '../adapter';
 import * as Zcl from '../zcl';
 import {Wait} from '../utils';
 import Debug from "debug";
-import events from 'events';
 
 const debug = Debug('zigbee-herdsman:controller:touchlink');
 
@@ -24,48 +23,44 @@ const resetFactoryNewRequest = Zcl.ZclFrame.create(
     {transactionID: 1}
 );
 
-const scanChannels = [11]; //, 15, 20, 25];
+const scanChannels = [11, 15, 20, 25, 12, 13, 14, 16, 17, 18, 19, 21, 22, 23, 24, 26];
 
-class Touchlink extends events.EventEmitter {
+class Touchlink {
     private adapter: Adapter;
 
     public constructor(adapter: Adapter) {
-        super();
         this.adapter = adapter;
     }
 
-    public async scanAndFactoryReset(): Promise<void> {
+    public async factoryReset(): Promise<void> {
         let done = false;
 
         for (const channel of scanChannels) {
             debug(`Set InterPAN channel to '${channel}'`);
             await this.adapter.setChannelInterPAN(channel);
 
-            for (let i = 0; i < 2; i++) {
-                try {
-                    await this.adapter.sendZclFrameInterPANWithResponse(scanRequest, 2000);
-                    debug(`Got scan response on channel '${channel}'`);
+            try {
+                await this.adapter.sendZclFrameInterPANWithResponse(scanRequest, 2000);
+                debug(`Got scan response on channel '${channel}'`);
 
-                    // Device answered (if not it will fall in the catch below),
-                    // identify it (this will make e.g. the bulb flash)
-                    debug(`Identifying`);
-                    await this.adapter.sendZclFrameInterPAN(identifyRequest);
-                    await Wait(2000);
+                // Device answered (if not it will fall in the catch below),
+                // identify it (this will make e.g. the bulb flash)
+                debug(`Identifying`);
+                await this.adapter.sendZclFrameInterPAN(identifyRequest);
+                await Wait(2000);
 
-                    debug(`Reset to factory new`);
-                    await this.adapter.sendZclFrameInterPAN(resetFactoryNewRequest);
-                    done = true;
-                    break;
-                } catch (error) {
-                    debug(`Scan request failed or was not answered: '${error}'`);
-                }
+                debug(`Reset to factory new`);
+                await this.adapter.sendZclFrameInterPAN(resetFactoryNewRequest);
+                done = true;
+            } catch (error) {
+                debug(`Scan request failed or was not answered: '${error}'`);
             }
+
+            debug(`Restore InterPAN channel`);
+            await this.adapter.restoreChannelInterPAN();
 
             if (done) break;
         }
-
-        debug(`Restore InterPAN channel`);
-        await this.adapter.restoreChannelInterPAN();
     }
 }
 
