39ad64dbddf2a84fb230e3210db0abb02e42d121
Ilya Kirov <kirovilya@gmail.com>
2019-12-04 22:40:18 +0300

    2 attempts to get a list of active endpoints (#103)
    * 2 attempts to get a list of active endpoints
    
    * Update device.ts
    
    * Update device.ts

--- src/controller/model/device.ts
+++ src/controller/model/device.ts
@@ -337,7 +337,21 @@ class Device extends Entity {
             }
         }
 
-        const activeEndpoints = await Entity.adapter.activeEndpoints(this.networkAddress);
+        // e.g. Xiaomi Aqara Opple devices fail to respond to the first active endpoints request, therefore try 2 times
+        // https://github.com/Koenkk/zigbee-herdsman/pull/103
+        let activeEndpoints;
+        for (let attempt = 0; attempt < 2; attempt++) {
+            try {
+                activeEndpoints = await Entity.adapter.activeEndpoints(this.networkAddress);
+                break;
+            } catch (error) {
+                debug(`Interview - active endpoints request failed for '${this.ieeeAddr}', attempt ${attempt + 1}`);
+            }
+        }
+        if (!activeEndpoints) {
+            throw new Error(`Interview failed because can not get active endpoints ('${this.ieeeAddr}')`);
+        }
+
         // Some devices, e.g. TERNCY return endpoint 0 in the active endpoints request.
         // This is not a valid endpoint number according to the ZCL, requesting a simple descriptor will result
         // into an error. Therefore we filter it, more info: https://github.com/Koenkk/zigbee-herdsman/issues/82
