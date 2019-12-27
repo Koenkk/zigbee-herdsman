871d17276c5d533cab916eb34eb1f82479bed814
Carl de Billy <carl.debilly@gmail.com>
2019-12-27 14:31:37 -0500

    Removed now unused branch (failing test coverage)

--- src/zcl/zclFrame.ts
+++ src/zcl/zclFrame.ts
@@ -320,8 +320,6 @@ class ZclFrame {
             const failedCondition = parameter.conditions.find((condition): boolean => {
                 if (condition.type === 'statusEquals') {
                     return entry.status !== condition.value;
-                } else if (condition.type == 'statusNotEquals') {
-                    return entry.status === condition.value;
                 } else if (condition.type == 'directionEquals') {
                     return entry.direction !== condition.value;
                 } else  {
