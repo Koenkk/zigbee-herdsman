a274cef6156a8c7eebbda80360bc21f578f77f18
Anthony Nichols <hi@anthonynichols.me>
2020-01-09 14:17:01 -0600

    Include type definitions in build

--- index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-    Controller: require('./dist/controller/controller.js').default,
-    Zcl: require('./dist/zcl/index.js'),
-};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import {default as Controller} from './controller/controller';
+import * as Zcl from './zcl';
+
+export {Controller};
+export {Zcl};
