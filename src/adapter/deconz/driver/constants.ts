358d63af08462ea41da12d75877a51321918e205
ChrisHae <cha@dresden-elektronik.de>
2020-01-09 10:36:14 +0100

    Added constants for APS layer

--- src/adapter/deconz/driver/constants.ts
+++ src/adapter/deconz/driver/constants.ts
@@ -12,9 +12,22 @@ const PARAM: {
         CHANNEL: 0x1c
     },
     FrameType: {
+        ReadDeviceState: 0x07,
         ReadParameter: 0x0a,
         WriteParameter: 0x0b,
-        ReadFirmwareVersion: 0x0d
+        ReadFirmwareVersion: 0x0d,
+        DeviceStateChanged: 0x0e
+    },
+    APS: {
+        DATA_CONFIRM: 0x04,
+        DATA_REQUEST: 0x12,
+        DATA_INDICATION: 0x17
+    },
+    NetworkState: {
+        NET_OFFLINE: 0x00,
+        NET_JOINING: 0x01,
+        NET_CONNECTED: 0x02,
+        NET_LEAVING: 0x03,
     }
 }
 
@@ -22,10 +35,54 @@ interface Request {
     commandId?: number;
     parameterId?: number;
     parameter?: parameterT;
+    request?: object;
     seqNumber?: number;
     resolve?: Function;
     reject?: Function;
     ts?: number;
+}
+
+interface ReceivedDataResponse {
+    commandId?: number;
+    seqNr?: number;
+    status?: number;
+    frameLength?: number;
+    payloadLength?: number;
+    deviceState?: number;
+    destAddrMode?: number;
+    destAddr16?: number;
+    destAddr64?: string;
+    destEndpoint?: number;
+    srcAddrMode?: number;
+    srcAddr16?: number;
+    srcAddr64?: string;
+    srcEndpoint?: number;
+    profileId?: number;
+    clusterId?: number;
+    asduLength?: number;
+    asduPayload?: number[];
+    lqi?: number;
+    rssi?: number;
+}
+
+interface ApsDataRequest {
+    commandId?: number;
+    seqNr?: number;
+    frameLength?: number;
+    payloadLength?: number;
+    requestId?: number;
+    flags?: number;
+    destAddrMode?: number;
+    destAddr16?: number;
+    destAddr64?: number[];
+    destEndpoint?: number;
+    profileId?: number;
+    clusterId?: number;
+    srcEndpoint?: number;
+    asduLength?: number;
+    asduPayload?: number[];
+    txOptions?: number;
+    radius?: number;
 }
 
 type ParamMac = string;
@@ -38,6 +95,6 @@ type ParamChannelMask = number;
 type Command = ParamMac | ParamPanId | ParamNwkAddr | ParamExtPanId | ParamChannel |ParamChannelMask;
 type parameterT = number | number[];
 
-export { Request, parameterT , Command, ParamMac, ParamPanId, ParamNwkAddr, ParamExtPanId, ParamChannel, ParamChannelMask };
+export { Request, ApsDataRequest, ReceivedDataResponse, parameterT , Command, ParamMac, ParamPanId, ParamNwkAddr, ParamExtPanId, ParamChannel, ParamChannelMask };
 
 export default {PARAM};
