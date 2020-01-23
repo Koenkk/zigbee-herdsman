cc17e81403b164f8f7c5880f02dbbbf57878fa2a
Koen Kanters <koenkanters94@gmail.com>
2020-01-23 20:09:27 +0100

    Update zStackAdapter.test.ts

--- test/adapter/z-stack/zStackAdapter.test.ts
+++ test/adapter/z-stack/zStackAdapter.test.ts
@@ -668,6 +668,66 @@ describe('zStackAdapter', () => {
         expect(mockZnpRequest.mock.calls[15][2].value).toStrictEqual(Buffer.from([0x55]));
     });
 
+    it('Start zStack 1.2 when it doesnt support version call (on old firmwares)', async () => {
+        // https://github.com/Koenkk/zigbee-herdsman/issues/129
+        networkOptions.networkKeyDistribute = true;
+        adapter = new ZStackAdapter(networkOptions, serialPortOptions, 'backup.json');
+        mockZnpRequest.mockImplementation(async (subsystem, command, payload, expectedStatus) => {
+            const missing = () => {
+                const msg = `Not implemented - ${Subsystem[subsystem]} - ${command} - ${JSON.stringify(payload)}`;
+                console.log(msg)
+                throw new Error(msg);
+            }
+
+            if (subsystem === Subsystem.SYS && command === 'version' && equals(payload, {})) {
+                throw new Error('Not implemented');
+            } else if (subsystem === Subsystem.SYS && command === 'osalNvRead') {
+                if (equalsPartial(payload, {id: NvItemsIds.ZNP_HAS_CONFIGURED_ZSTACK1, offset: 0})) {
+                    return {payload: {value: Buffer.from([0x00])}};
+                } else {
+                    missing();
+                }
+            } else if (subsystem === Subsystem.UTIL && command === 'getDeviceInfo') {
+                return {payload: {devicestate: Constants.COMMON.devStates.ZB_COORD}};
+            } else if (subsystem === Subsystem.ZDO && command === 'activeEpReq') {
+                return {};
+            } else if (subsystem === Subsystem.AF && command === 'register') {
+                return {};
+            } else if (subsystem === Subsystem.SYS && command === 'resetReq') {
+                return {};
+            } else if (subsystem === Subsystem.SYS && command === 'osalNvWrite') {
+                return {};
+            } else if (subsystem === Subsystem.SYS && command === 'osalNvItemInit') {
+                return {};
+            } else if (subsystem === Subsystem.SAPI && command === 'readConfiguration') {
+                return {payload: {value: Buffer.from(networkOptions.networkKey)}};
+            } else if (subsystem === Subsystem.SAPI && command === 'writeConfiguration') {
+                return {};
+            } else {
+                missing();
+            }
+        });
+
+        mockZnpWaitfor.mockImplementation((type, subsystem, command, payload) => {
+            const missing = () => {
+                const msg = `Not implemented - ${Type[type]} - ${Subsystem[subsystem]} - ${command} - ${JSON.stringify(payload)}`;
+                console.log(msg)
+                throw new Error(msg);
+            }
+
+            if (type === Type.AREQ && subsystem === Subsystem.ZDO && command === 'activeEpRsp') {
+                return {promise: {payload: {activeeplist: []}}};
+            } else if (type === Type.AREQ && subsystem === Subsystem.ZDO && command === 'stateChangeInd') {
+                return {promise: {payload: {activeeplist: []}}};
+            } else {
+                missing();
+            }
+        });
+
+        const result = await adapter.start();
+        expect(result).toBe('restored');
+    });
+
     it('Start zStack 3.0.x initialize', async () => {
         mockZnpRequest.mockImplementation((subsystem, command, payload, expectedStatus) => {
             const missing = () => {
