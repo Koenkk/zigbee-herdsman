c50bf28bd962d3a4f421209286050274b8efa2f9
Ilya Kirov <kirovilya@gmail.com>
2020-01-23 22:11:15 +0300

    #129 catch parsing wrong firmware version (#130)
    * #129 catch parsing wrong firmware version
    
    Old firmware did not contain version information and parse with error now
    
    * Update zStackAdapter.ts
    
    * Update zStackAdapter.ts
    
    * Update zStackAdapter.test.ts
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/adapter/z-stack/adapter/zStackAdapter.ts
+++ src/adapter/z-stack/adapter/zStackAdapter.ts
@@ -82,7 +82,13 @@ class ZStackAdapter extends Adapter {
     public async start(): Promise<StartResult> {
         await this.znp.open();
 
-        this.version = (await this.znp.request(Subsystem.SYS, 'version', {})).payload;
+        // Old firmware did not support version, assume it's Z-Stack 1.2 for now.
+        try {
+            this.version = (await this.znp.request(Subsystem.SYS, 'version', {})).payload;
+        } catch (e) {
+            this.version = {"transportrev":2, "product":0, "majorrel":2, "minorrel":0, "maintrel":0, "revision":""};
+        }
+
         debug(`Detected znp version '${ZnpVersion[this.version.product]}' (${JSON.stringify(this.version)})`);
 
         return StartZnp(this.znp, this.version.product, this.networkOptions, this.backupPath);
