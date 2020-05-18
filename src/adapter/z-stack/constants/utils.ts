362be894d961259d63be6fc8e317036ccc496945
Roman Vyakhirev <roman.vyakhirev@gmail.com>
2020-05-18 23:56:11 +0200

    Calculate channel mask instead of decimal magic values
    Looks like NONE and CH_ALL are not in use hence ignore them.

--- src/adapter/z-stack/constants/utils.ts
+++ src/adapter/z-stack/constants/utils.ts
@@ -1,15 +1,5 @@
-import Common from './common';
-
 function getChannelMask(channels: number[]): number[] {
-    let value = 0;
-
-    for (const channel of channels) {
-        for (const [key, logicalChannel] of Object.entries(Common.logicalChannels)) {
-            if (logicalChannel === channel) {
-                value = value | Common.channelMask[key];
-            }
-        }
-    }
+    const value = channels.reduce((mask, channel) => mask | (1 << channel), 0);
 
     return [value & 0xFF, (value >> 8) & 0xFF, (value >> 16) & 0xFF, (value >> 24) & 0xFF];
 }
