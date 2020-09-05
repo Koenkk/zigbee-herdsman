b1d85c72404a2f0a8473e1c9f5d6d7ebe6c903a7
Kris B <krisbaumann@gmail.com>
2020-09-05 20:46:53 +0200

    Revert "fix lint"
    This reverts commit 30d32677892762f9238e2bb2a822b7f7ecbef515.

--- src/controller/events.ts
+++ src/controller/events.ts
@@ -92,8 +92,7 @@ type MessagePayloadType =
     'commandUpOpen' | 'commandDownClose' | 'commandMoveToLevel' | 'commandMoveColorTemp' | 'commandGetData' |
     'commandSetDataResponse' | 'commandGetWeeklyScheduleRsp' | 'commandQueryNextImageRequest' | 'commandNotification' |
     'commandAlertsNotification' | 'commandProgrammingEventNotification' | "commandGetPinCodeRsp" |
-    "commandArrivalSensorNotify" | 'commandSceneAddRsp' | 'commandSceneViewRsp' | 'commandSceneRemoveRsp' |
-    'commandSceneRemoveAllRsp' | 'commandSceneStoreRsp' | 'commandScenegetSceneMembershipRsp';
+    "commandArrivalSensorNotify" | 'commandSceneAddRsp' | 'commandSceneViewRsp' | 'commandSceneRemoveRsp' | 'commandSceneRemoveAllRsp' | 'commandSceneStoreRsp' | 'commandScenegetSceneMembershipRsp';
 
 interface MessagePayload {
     type: MessagePayloadType;
