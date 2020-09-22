385e1097c1752bb129d86aa9c5ff5a02fc7f8e11
ChrisHae <cha@dresden-elektronik.de>
2020-09-22 14:57:48 +0200

    Hotfix for error: "Could not parse ZclFrame" during Interview

--- src/adapter/deconz/adapter/deconzAdapter.ts
+++ src/adapter/deconz/adapter/deconzAdapter.ts
@@ -1008,12 +1008,15 @@ class DeconzAdapter extends Adapter {
     private checkReceivedDataPayload(resp: ReceivedDataResponse) {
         let srcAddr: any = null;
         let frame: ZclFrame = null;
+
         if (resp != null) {
             srcAddr = (resp.srcAddr16 != null) ? resp.srcAddr16 : resp.srcAddr64;
-            try {
-                frame = ZclFrame.fromBuffer(resp.clusterId, Buffer.from(resp.asduPayload));
-            } catch (error) {
-                debug("could not parse zclFrame: " + error);
+            if (resp.profileId != 0x00) {
+                try {
+                    frame = ZclFrame.fromBuffer(resp.clusterId, Buffer.from(resp.asduPayload));
+                } catch (error) {
+                    debug("could not parse zclFrame: " + error);
+                }
             }
         }
 
