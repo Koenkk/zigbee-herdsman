01907a304fa25a3b73f336fac08e81ccbba97fbe
Ilya Kirov <kirovilya@gmail.com>
2020-09-23 21:09:44 +0300

    mgmtNWKUpdateNotify response

--- src/adapter/z-stack/znp/definition.ts
+++ src/adapter/z-stack/znp/definition.ts
@@ -1854,6 +1854,20 @@ const Definition: {
             ],
         },
         {
+            name: 'mgmtNWKUpdateNotify',
+            ID: 184,
+            type: CommandType.AREQ,
+            request: [
+                {name: 'srcaddr', parameterType: ParameterType.UINT16},
+                {name: 'status', parameterType: ParameterType.UINT8},
+                {name: 'scannedchannels', parameterType: ParameterType.UINT32},
+                {name: 'totaltrans', parameterType: ParameterType.UINT16},
+                {name: 'transFails', parameterType: ParameterType.UINT16},
+                {name: 'energylenght', parameterType: ParameterType.UINT8},
+                {name: 'energyvalues', parameterType: ParameterType.LIST_UINT8},
+            ],
+        },
+        {
             name: 'stateChangeInd',
             ID: 192,
             type: CommandType.AREQ,
