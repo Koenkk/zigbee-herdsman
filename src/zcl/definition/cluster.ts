6cf52dbfbfc49909d581453c945f51fc75b5113c
Schutt <markus@maglo.at>
2020-10-13 22:26:23 +0200

    Fix linter issues

--- src/zcl/definition/cluster.ts
+++ src/zcl/definition/cluster.ts
@@ -2040,8 +2040,8 @@ const Cluster: {
             colorCapabilities: {ID: 16394, type: DataType.uint16},
             colorTempPhysicalMin: {ID: 16395, type: DataType.uint16},
             colorTempPhysicalMax: {ID: 16396, type: DataType.uint16},
-            tuyaBrightness: { ID: 61441, type: DataType.uint16 },
-            tuyaRgbMode: { ID: 61440, type: DataType.uint16 }
+            tuyaBrightness: {ID: 61441, type: DataType.uint16},
+            tuyaRgbMode: {ID: 61440, type: DataType.uint16},
         },
         commands: {
             moveToHue: {
@@ -2092,10 +2092,10 @@ const Cluster: {
             moveToHueAndSaturationBrightness: {
                 ID: 6,
                 parameters: [
-                    { name: 'hue', type: DataType.uint8 },
-                    { name: 'saturation', type: DataType.uint8 },
-                    { name: 'transtime', type: DataType.uint16 },
-                    { name: 'brightness', type: DataType.uint8 }
+                    {name: 'hue', type: DataType.uint8},
+                    {name: 'saturation', type: DataType.uint8},
+                    {name: 'transtime', type: DataType.uint16},
+                    {name: 'brightness', type: DataType.uint8},
                 ],
             },
             moveToHueAndSaturation: {
@@ -2210,7 +2210,7 @@ const Cluster: {
             tuyaRgbMode: {
                 ID: 240,
                 parameters: [
-                    { name: 'enable', type: DataType.uint8 }
+                    {name: 'enable', type: DataType.uint8},
                 ]
             },
         },
