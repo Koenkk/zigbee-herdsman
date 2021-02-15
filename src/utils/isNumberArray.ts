eb1eefdc6c21ad5e6f7cdff38692853dd3dc32ef
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-02-15 18:30:20 +0100

    Trivial simplification of util code. (#311)
    * Trivial simplification of util code.
    
    * equalPartial is generic in first argument.

--- src/utils/isNumberArray.ts
+++ src/utils/isNumberArray.ts
@@ -1,16 +1,5 @@
-// eslint-disable-next-line
-function isNumberArray(value: any): value is number[] {
-    if (value instanceof Array) {
-        for (const item of value) {
-            if (typeof item !== 'number') {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    return false;
+function isNumberArray(value: unknown): value is number[] {
+    return value instanceof Array && value.every(item => typeof item === 'number');
 }
 
 export default isNumberArray;
