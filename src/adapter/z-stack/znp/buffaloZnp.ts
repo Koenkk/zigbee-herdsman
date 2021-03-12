d6cb9b9c26c0979e45590dbad1ad721380a8174f
Peter Brottveit Bock <post@peterbb.net>
2021-03-12 23:01:47 +0100

    Narrow buffalo-types
    Several occurences of the any-in-disguise Value-type in Buffalo
    can be replaced by their actual type without any change in
    the code that is executed.
    
    Fixes small bugs revealed by this.

--- src/adapter/z-stack/znp/buffaloZnp.ts
+++ src/adapter/z-stack/znp/buffaloZnp.ts
@@ -2,98 +2,135 @@
 import {Buffalo, TsType} from '../../../buffalo';
 import {BuffaloZnpOptions} from './tstype';
 
-class BuffaloZnp extends Buffalo {
-    private readListRoutingTable(options: TsType.Options): TsType.Value {
-        const statusLookup: {[n: number]: string} = {
-            0: 'ACTIVE',
-            1: 'DISCOVERY_UNDERWAY',
-            2: 'DISCOVERY_FAILED',
-            3: 'INACTIVE',
-        };
 
+type RoutingTableEntryStatus = 'ACTIVE' | 'DISCOVERY_UNDERWAY' | 'DISCOVERY_FAILED' | 'INACTIVE';
+
+const routingTableStatusLookup: Record<number, RoutingTableEntryStatus> = {
+    0: 'ACTIVE',
+    1: 'DISCOVERY_UNDERWAY',
+    2: 'DISCOVERY_FAILED',
+    3: 'INACTIVE',
+};
+
+interface RoutingEntry {
+    destNwkAddr: number;
+    routeStatus: RoutingTableEntryStatus | undefined;
+    nextHopNwkAddr: number;
+}
+
+interface Bind {
+    srcAddr: string;
+    srcEp: number;
+    clusterId: number;
+    dstAddrMode: number;
+    dstAddr: string;
+    dstEp?: number;
+}
+
+interface Neighbor {
+    extPandId: string;
+    extAddr: string;
+    nwkAddr: number
+    deviceType: number;
+    rxOnWhenIdle: number;
+    relationship: number;
+    permitJoin: number;
+    depth: number;
+    lqi: number;
+}
+
+interface Network {
+    neightborPanId: number;
+    logicalChannel: number;
+    stackProfile: number;
+    zigbeeVersion: number;
+    beaconOrder: number;
+    superFrameOrder: number;
+    permitJoin: number;
+}
+
+
+class BuffaloZnp extends Buffalo {
+    private readListRoutingTable(options: TsType.Options): RoutingEntry[] {
         const value = [];
+
         for (let i = 0; i < options.length; i++) {
             value.push({
-                'destNwkAddr': this.readUInt16(),
-                'routeStatus': statusLookup[this.readUInt8()],
-                'nextHopNwkAddr': this.readUInt16(),
+                destNwkAddr: this.readUInt16(),
+                routeStatus: routingTableStatusLookup[this.readUInt8()],
+                nextHopNwkAddr: this.readUInt16(),
             });
         }
 
         return value;
     }
 
-    private readListBindTable(options: TsType.Options): TsType.Value {
+    private readListBindTable(options: TsType.Options): Bind[]{
         const value = [];
 
         for (let i = 0; i < options.length; i++) {
-            const item: {[s: string]: number|string} = {};
-
-            item['srcAddr'] = this.readIeeeAddr();
-            item['srcEp'] = this.readUInt8();
-            item['clusterId'] = this.readUInt16();
-            item['dstAddrMode'] = this.readUInt8();
-            item['dstAddr'] = this.readIeeeAddr();
-
+            const item: Bind = {
+                srcAddr: this.readIeeeAddr(),
+                srcEp: this.readUInt8(),
+                clusterId: this.readUInt16(),
+                dstAddrMode: this.readUInt8(),
+                dstAddr: this.readIeeeAddr(),
+            };
             if (item.dstAddrMode === 3) {
-                item['dstEp'] = this.readUInt8();
+                item.dstEp = this.readUInt8();
             }
-
             value.push(item);
+        }
+        return value;
+    }
+
+    private readListNeighborLqi(options: TsType.Options): Neighbor[] {
+        const value = [];
+        for (let i = 0; i < options.length; i++) {
+            const prefix = {
+                extPandId: this.readIeeeAddr(),
+                extAddr: this.readIeeeAddr(),
+                nwkAddr: this.readUInt16()
+            };
+            const bitfields = this.readUInt8();
+            value.push({
+                ...prefix,
+                deviceType: bitfields & 0x03,
+                rxOnWhenIdle: (bitfields & 0x0C) >> 2,
+                relationship: (bitfields & 0x70) >> 4,
+                permitJoin: this.readUInt8() & 0x03,
+                depth: this.readUInt8(),
+                lqi: this.readUInt8()
+            });
         }
 
         return value;
     }
 
-    private readListNeighborLqi(options: TsType.Options): TsType.Value {
+    private  readListNetwork(options: TsType.Options): Network[] {
         const value = [];
         for (let i = 0; i < options.length; i++) {
-            const item: {[s: string]: number|string} = {};
-
-            item['extPandId'] = this.readIeeeAddr();
-            item['extAddr'] = this.readIeeeAddr();
-            item['nwkAddr'] = this.readUInt16();
-
+            const neightborPanId = this.readUInt16();
+            const logicalChannel = this.readUInt8();
             const value1 = this.readUInt8();
-            item['deviceType'] = value1 & 0x03;
-            item['rxOnWhenIdle'] = (value1 & 0x0C) >> 2;
-            item['relationship'] = (value1 & 0x70) >> 4;
-
-            item['permitJoin'] = this.readUInt8() & 0x03;
-            item['depth'] = this.readUInt8();
-            item['lqi'] = this.readUInt8();
-
-            value.push(item);
-        }
-
-        return value;
-    }
-
-    private  readListNetwork(options: TsType.Options): TsType.Value {
-        const value = [];
-        for (let i = 0; i < options.length; i++) {
-            const item: {[s: string]: number|string} = {};
-
-            item['neightborPanId'] = this.readUInt16();
-            item['logicalChannel'] = this.readUInt8();
-
-            const value1 = this.readUInt8();
-            item['stackProfile'] = value1 & 0x0F;
-            item['zigbeeVersion'] = (value1 & 0xF0) >> 4;
-
             const value2 = this.readUInt8();
-            item['beaconOrder'] = value2 & 0x0F;
-            item['superFrameOrder'] = (value2 & 0xF0) >> 4;
+            const permitJoin = this.readUInt8();
 
-            item['permitJoin'] = this.readUInt8();
-
-            value.push(item);
+            value.push({
+                neightborPanId,
+                logicalChannel,
+                stackProfile: value1 & 0x0F,
+                zigbeeVersion: (value1 & 0xF0) >> 4,
+                beaconOrder: value2 & 0x0F,
+                superFrameOrder: (value2 & 0xF0) >> 4,
+                permitJoin
+            });
         }
 
         return value;
     }
 
-    private readListAssocDev(options: BuffaloZnpOptions): TsType.Value {
+    private readListAssocDev(options: BuffaloZnpOptions): number[] {
         const value = [];
         const listLength = options.length - options.startIndex;
 
