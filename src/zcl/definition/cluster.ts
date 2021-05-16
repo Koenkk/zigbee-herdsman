9b5912a80f859d63e8bcde05cbe1645a929bacf5
Koen Kanters <koenkanters94@gmail.com>
2021-05-16 16:47:23 +0200

    Update cluster.ts

--- src/zcl/definition/cluster.ts
+++ src/zcl/definition/cluster.ts
@@ -1912,7 +1912,7 @@ const Cluster: {
             viessmannCustom13: {ID: 0x404c, type: DataType.enum8, manufacturerCode: ManufacturerCode.VIESSMAN_ELEKTRO},
             viessmannCustom14: {ID: 0x404d, type: DataType.bitmap8, manufacturerCode: ManufacturerCode.VIESSMAN_ELEKTRO},
             viessmannCustom15: {ID: 0x404e, type: DataType.bitmap8, manufacturerCode: ManufacturerCode.VIESSMAN_ELEKTRO},
-            schneiderWiserSpecific: { ID: 0xe110, type: dataType_1.default.enum8, manufacturerCode: manufacturerCode_1.default.SCHNEIDER },
+            schneiderWiserSpecific: {ID: 0xe110, type: DataType.enum8, manufacturerCode: ManufacturerCode.SCHNEIDER},
         },
         commands: {
             setpointRaiseLower: {
@@ -1951,10 +1951,10 @@ const Cluster: {
             schneiderWiserThermostatBoost: {
                 ID: 0x80,
                 parameters: [
-                    { name: 'command', type: dataType_1.default.enum8 },
-                    { name: 'enable', type: dataType_1.default.enum8 },
-                    { name: 'temperature', type: dataType_1.default.uint16 },
-                    { name: 'duration', type: dataType_1.default.uint16 },
+                    {name: 'command', type: DataType.enum8},
+                    {name: 'enable', type: DataType.enum8},
+                    {name: 'temperature', type: DataType.uint16},
+                    {name: 'duration', type: DataType.uint16},
                 ],
             },
         },
