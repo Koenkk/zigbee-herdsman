c2d81911ffd1dfa0d89889ccc2c6e914d75b1c57
embak <embak@users.noreply.github.com>
2023-01-25 16:15:20 -0500

    Update Sinope RM3500ZB specific attributes.

--- src/zcl/definition/cluster.ts
+++ src/zcl/definition/cluster.ts
@@ -4099,9 +4099,9 @@ const Cluster: {
             ecoMode1: {ID: 114, type: DataType.uint8}, // default:255||0-99
             ecoMode2: {ID: 115, type: DataType.uint8}, // default 255||0-100
             unknown: {ID: 117, type: DataType.bitmap32}, // RW *testing*
-            waterHeaterTemperatureMin: {ID: 118, type: DataType.uint8},
-            waterHeaterDRDuration: {ID: 119, type: DataType.uint8},
-            waterHeaterDRTimeOn: {ID: 120, type: DataType.uint16},
+            DRWaterHeaterTempMin: {ID: 118, type: DataType.uint8}, // RW *testing*
+            DRWaterHeaterTempTime: {ID: 119, type: DataType.uint8}, // RW *testing*
+            DRWaterHeaterTimeOn: {ID: 120, type: DataType.uint16}, // RW *testing*
             unknown1: {ID: 128, type: DataType.uint32}, // readOnly stringNumber *testing*
             dimmerTimmer: {ID: 160, type: DataType.uint32},
             unknown2: {ID: 256, type: DataType.uint8}, // readOnly *testing*
@@ -4122,7 +4122,7 @@ const Cluster: {
             // attribute ID: 300's readable, returns a buffer
             reportLocalTemperature: {ID: 301, type: DataType.int16},
             // attribute ID: 512's readable
-            coldLoadPickUpStatus: {ID: 643, type: DataType.uint16},
+            coldLoadPickupStatus: {ID: 643, type: DataType.enum8}, // readOnly *testing*
         },
         commands: {
         },
