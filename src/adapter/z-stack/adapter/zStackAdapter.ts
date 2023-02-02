7530ba082c92d259cf7a0c6935243a0974b05c0b
Tom Tonic <tom@tomtonic.de>
2023-02-02 17:23:45 +0100

    Improve debug logging for routing table/LQI request failure (#651)
    * added more debugging info for LQI/routingTable
    
    * updated test to match new failure message
    
    * reduced line lengths
    
    * fixed indentation
    
    * switched positions of text and code in debug msg
    
    * adapted test to new error message
    
    * adapted error message
    
    * fixed error message
    
    * added LQI/routing debugging info to EZSP adapter
    
    * fixed syntax
    
    * fixed syntax
    
    * removed error code resolution
    
    does not work due to some indexing problem
    
    * Update adapter.test.ts
    
    * Update zStackAdapter.ts
    
    ---------
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/adapter/z-stack/adapter/zStackAdapter.ts
+++ src/adapter/z-stack/adapter/zStackAdapter.ts
@@ -564,7 +564,8 @@ class ZStackAdapter extends Adapter {
                 );
                 const result = await response.start().promise;
                 if (result.payload.status !== ZnpCommandStatus.SUCCESS) {
-                    throw new Error(`LQI for '${networkAddress}' failed`);
+                    throw new Error(`LQI for '${networkAddress}' failed with error: '${
+                        ZnpCommandStatus[result.payload.status]}' (${result.payload.status})`);
                 }
 
                 return result;
@@ -611,7 +612,8 @@ class ZStackAdapter extends Adapter {
                 );
                 const result = await response.start().promise;
                 if (result.payload.status !== ZnpCommandStatus.SUCCESS) {
-                    throw new Error(`Routing table for '${networkAddress}' failed`);
+                    throw new Error(`Routing table for '${networkAddress}' failed with error: '${
+                        ZnpCommandStatus[result.payload.status]}' (${result.payload.status})`);
                 }
 
                 return result;
