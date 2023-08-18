9597a0a5dd0833e9228a13fe79fc82a2d4579ef8
koenkk <koenkanters94@gmail.com>
2023-08-18 19:47:43 +0200

    chore: fix typo

--- src/adapter/z-stack/adapter/adapter-backup.ts
+++ src/adapter/z-stack/adapter/adapter-backup.ts
@@ -197,7 +197,7 @@ export class AdapterBackup {
             /**
              * Due to a bug in ZStack, some devices go missing from the backed-up device tables which makes them disappear from the backup.
              * This causes the devices not to be restored when e.g. re-flashing the adapter.
-             * If you then try to join a new device via a Zigbee 3.0 router that went missing (those with a linkkey), joning fails as the coordinator
+             * If you then try to join a new device via a Zigbee 3.0 router that went missing (those with a linkkey), joining fails as the coordinator
              * does not have the linkKey anymore.
              * Below we don't remove any devices from the backup which have a linkkey and are still in the database (=ieeeAddressesInDatabase)
              */
