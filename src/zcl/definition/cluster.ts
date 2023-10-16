5b854d1f404fcc42c2249b961ddde7fa652074bb
Nathan Fiscus <nathanfiscus@users.noreply.github.com>
2023-10-16 11:43:14 -0600

    feat: add Inovelli VZM35 cluster parameters (#780)
    * Add support for Inovelli LZW31-SN manufacturer specific cluster
    
    * update cluster def
    
    * update parameters to camelCase
    
    * Update cluster.ts
    
    * add vzm3335 parameters and new firmware params
    
    ---------
    
    Co-authored-by: Inovelli <37669481+InovelliUSA@users.noreply.github.com>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/zcl/definition/cluster.ts
+++ src/zcl/definition/cluster.ts
@@ -5040,7 +5040,7 @@ const Cluster: {
         },
         commandsResponse: {},
     },
-    manuSpecificInovelliVZM31SN: {
+    manuSpecificInovelli: {
         ID: 64561,
         manufacturerCode: 0x122f,
         attributes: {
@@ -5061,11 +5061,16 @@ const Cluster: {
             stateAfterPowerRestored: {ID: 0x000f, type: DataType.uint8},
             loadLevelIndicatorTimeout: {ID: 0x0011, type: DataType.uint8},
             activePowerReports: {ID: 0x0012, type: DataType.uint8},
+
             periodicPowerAndEnergyReports: {ID: 0x0013, type: DataType.uint16},
             activeEnergyReports: {ID: 0x0014, type: DataType.uint16},
             powerType: {ID: 0x0015, type: DataType.boolean},
             switchType: {ID: 0x0016, type: DataType.uint8},
             higherOutputInNonNeutral: {ID: 0x0019, type: DataType.boolean},
+            nonNeutralAuxMediumGear: {ID: 0x001e, type: DataType.uint8},
+            nonNeutralAuxLowGear: {ID: 0x001f, type: DataType.uint8},
+            internalTemperature: {ID: 0x0020, type: DataType.uint8},
+            overheat: {ID: 0x0021, type: DataType.boolean},
             buttonDelay: {ID: 0x0032, type: DataType.uint8},
             deviceBindNumber: {ID: 0x0033, type: DataType.uint8},
             smartBulbMode: {ID: 0x0034, type: DataType.boolean},
@@ -5115,6 +5120,7 @@ const Cluster: {
             firmwareUpdateInProgressIndicator: {ID: 0x0104, type: DataType.boolean},
             relayClick: {ID: 0x105, type: DataType.boolean},
             doubleTapClearNotifications: {ID: 0x106, type: DataType.boolean},
+            fanLedLevelType: {ID: 0x0107, type: DataType.uint8},
         },
         commands: {
             ledEffect: {
