f98291209cf4014e1499c894a86dc93636e14dcd
AlexisPolegato <alexis.polegato@gmail.com>
2023-11-03 00:39:12 +0100

    Add NodOn Manuf Code and Manufacturer Cluster

--- src/zcl/definition/manufacturerCode.ts
+++ src/zcl/definition/manufacturerCode.ts
@@ -1060,6 +1060,8 @@ const knownManufacturerCodes = {
     ELKO: 0x1277,
     /** Sprut.device */
     SPRUT_DEVICE: 0x6666,
+    /** NodOn */
+    NODON: 0x128B,
 };
 
 export default {
@@ -1075,4 +1077,5 @@ export default {
     Develco: knownManufacturerCodes.DEVELCO,
     SprutDevice: knownManufacturerCodes.SPRUT_DEVICE,
     Bosch: knownManufacturerCodes.ROBERT_BOSCH_GMBH,
+    NodOn: knownManufacturerCodes.NODON,
 };
