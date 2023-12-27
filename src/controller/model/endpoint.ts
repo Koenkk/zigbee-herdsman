9951d44af24023ea339e159e6f86c942543b8546
Jorge Schrauwen <sjorge@blackdot.be>
2023-12-27 19:36:30 +0100

    fix: Fix wrong manufacturer code when configuring reporting for manufacturer specific attribute (#844)
    * fix: add test for ep.configureReporting with manufacturerCode
    
    When defining a manufacturerCode in the ZCL cluster definition, it should be used when configuring reporting, even when not specified.
    
    * fix: endpoint.configureReporting should use manufacturerCode when defined
    
    Fixes #843
    
    * Update endpoint.ts
    
    * Update endpoint.ts
    
    ---------
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/controller/model/endpoint.ts
+++ src/controller/model/endpoint.ts
@@ -633,6 +633,9 @@ class Endpoint extends Entity {
                     const attribute = cluster.getAttribute(item.attribute);
                     dataType = attribute.type;
                     attrId = attribute.ID;
+                    if (attribute.hasOwnProperty('manufacturerCode')) {
+                        options.manufacturerCode = attribute.manufacturerCode;
+                    }
                 }
             }
 
