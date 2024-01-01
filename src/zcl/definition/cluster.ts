7dcead952d455c39f3274451fd74d361858c485d
slugzero <2014249+slugzero@users.noreply.github.com>
2024-01-01 10:43:14 +0100

    feat: Add manuSpecificBosch10 Cluster for BMCT-SLZ (#849)
    * BMCT-SLZ: Add manuSpecificBosch10 cluster
    
    * BMCT-SLZ: change attributes to camel case

--- src/zcl/definition/cluster.ts
+++ src/zcl/definition/cluster.ts
@@ -5421,6 +5421,20 @@ const Cluster: {
         },
         commandsResponse: {},
     },
+    manuSpecificBosch10: {
+        ID: 0xfca0,
+        manufacturerCode: ManufacturerCode.Bosch,
+        attributes: {
+            deviceMode: {ID: 0x0000, type: DataType.enum8},
+            switchType: {ID: 0x0001, type: DataType.enum8},
+            calibrationOpeningTime: {ID: 0x0002, type: DataType.uint32},
+            calibrationClosingTime: {ID: 0x0003, type: DataType.uint32},
+            childLock: {ID: 0x0008, type: DataType.boolean},
+            motorState: {ID: 0x0013, type: DataType.enum8},
+        },
+        commands: {},
+        commandsResponse: {},
+    },
     manuSpecificAssaDoorLock: {
         ID: 0xFC00,
         attributes: {
