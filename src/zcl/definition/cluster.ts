adcfd61699833ac1aba9ffde62e441a11ac5e60c
Daniel N <Daniel.nagy@gmail.com>
2024-03-01 03:46:06 +1100

    feat: Add manuSpecificSchneiderFanSwitchConfiguration (#937)
    * Update cluster.ts
    
    Create new clipsalWiser Fan switch cluster for  https://github.com/Koenkk/zigbee2mqtt/issues/18620
    
    * Update cluster.ts
    
    * Update cluster.ts
    
    * Update cluster.ts
    
    web gui arguments - tabs vs spaces
    
    * Update cluster.ts
    
    standardise naming convention based on sjorge's comments.
    renamed attribute correctly to ledIndication

--- src/zcl/definition/cluster.ts
+++ src/zcl/definition/cluster.ts
@@ -5029,6 +5029,16 @@ const Cluster: {
         commands: {},
         commandsResponse: {},
     },
+    manuSpecificSchneiderFanSwitchConfiguration: {
+        ID: 0xFC04,
+        manufacturerCode: ManufacturerCode.SCHNEIDER,
+        attributes: {
+            ledIndication: {ID: 0x0002, type: DataType.uint8},
+            switchOrientation: {ID: 0x0060, type: DataType.uint8},
+        },
+        commands: {},
+        commandsResponse: {},
+    },
     sprutDevice: {
         ID: 26112,
         manufacturerCode: 26214,
