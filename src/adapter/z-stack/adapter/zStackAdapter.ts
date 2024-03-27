8b0641f862c406b0dba0163ceb37114ac87c0c5e
Andrew Berry <andrew@furrypaws.ca>
2024-03-27 19:40:20 -0400

    Explain why we send an event that doesn't match the command

--- src/adapter/z-stack/adapter/zStackAdapter.ts
+++ src/adapter/z-stack/adapter/zStackAdapter.ts
@@ -794,10 +794,21 @@ class ZStackAdapter extends Adapter {
 
                 this.emit(Events.Events.networkAddress, payload);
             } else if (object.command === 'concentratorIndCb') {
+                // Some routers may change short addresses and the announcement
+                // is missed by the coordinator. This can happen when there are
+                // power outages or other interruptions in service. They may
+                // not send additional announcements, causing the device to go
+                // offline. However, those devices may instead send
+                // Concentrator Indicator Callback commands, which contain both
+                // the short and the long address allowing us to update our own
+                // mappings.
+                // https://e2e.ti.com/cfs-file/__key/communityserver-discussions-components-files/158/4403.zstacktask.c
+                // https://github.com/Koenkk/zigbee-herdsman/issues/74
                 const payload: Events.NetworkAddressPayload = {
                     networkAddress: object.payload.srcaddr,
                     ieeeAddr: object.payload.extaddr,
                 };
+
                 this.emit(Events.Events.networkAddress, payload);
             } else {
                 /* istanbul ignore else */
