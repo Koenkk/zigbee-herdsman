1e1ab356bd2515297b59cff4f41e69dda3665e88
Andrew Berry <andrew@furrypaws.ca>
2024-03-27 19:40:38 -0400

    Test that we send the network update event on concentratorIndCb

--- test/adapter/z-stack/adapter.test.ts
+++ test/adapter/z-stack/adapter.test.ts
@@ -2781,6 +2781,16 @@ describe("zstack-adapter", () => {
         expect(networkAddress).toStrictEqual({ieeeAddr: '0x123', networkAddress: 124});
     });
 
+    it('Concentrator Callback Indication', async () => {
+        basicMocks();
+        await adapter.start();
+        let networkAddress;
+        const object = {type: Type.AREQ, subsystem: Subsystem.ZDO, command: 'concentratorIndCb', payload: {srcaddr: 124, extaddr: '0x123'}};
+        adapter.on("networkAddress", (p) => {networkAddress = p;})
+        znpReceived(object);
+        expect(networkAddress).toStrictEqual({ieeeAddr: '0x123', networkAddress: 124});
+    });
+
     it('Device leave', async () => {
         basicMocks();
         await adapter.start();
