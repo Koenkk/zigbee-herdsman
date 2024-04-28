d661d737b3e6668434879d7e998ab8d8541a94a9
Koen Kanters <koenkanters94@gmail.com>
2024-04-28 09:03:43 +0200

    fix(ignore): Export zspec https://github.com/Koenkk/zigbee-herdsman-converters/pull/7427

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import Controller from './controller/controller';
 import * as Zcl from './zcl';
+import * as ZSpec from './zspec';
 import * as logger from './utils/logger';
 
-export {Zcl, Controller};
+export {Zcl, Controller, ZSpec};
 
 /* istanbul ignore next */
 export const setLogger = logger.setLogger;
