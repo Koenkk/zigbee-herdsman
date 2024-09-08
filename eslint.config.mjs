2e948a96c311d9ad27670e8a6ee11c1d34f4598a
Koen Kanters <koenkanters94@gmail.com>
2024-09-08 21:51:04 +0200

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ export default tseslint.config(
     {
         languageOptions: {
             parserOptions: {
-                project: './tsconfig.json',
+                project: true,
             },
         },
         rules: {
