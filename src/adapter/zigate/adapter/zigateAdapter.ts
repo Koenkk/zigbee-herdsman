5d02efe44826401521b85259dde5e3ca4421e312
Nerivec <62446222+Nerivec@users.noreply.github.com>
2024-09-25 20:12:58 +0200

    fix(ignore): zigate: Workaround missing ZDO `LEAVE_RESPONSE` (#1204)

--- src/adapter/zigate/adapter/zigateAdapter.ts
+++ src/adapter/zigate/adapter/zigateAdapter.ts
@@ -229,8 +229,6 @@ class ZiGateAdapter extends Adapter {
                     prefixedPayload.set(payload, 0);
 
                     payload = prefixedPayload;
-                    // XXX: zigate is missing ZDO LEAVE_RESPONSE, force disable to avoid waitress timeout (LeaveIndication will do the rest)
-                    disableResponse = true;
                     break;
                 }
 
@@ -271,7 +269,10 @@ class ZiGateAdapter extends Adapter {
                 if (responseClusterId) {
                     waiter = this.driver.zdoWaitFor({
                         clusterId: responseClusterId,
-                        target: responseClusterId === Zdo.ClusterId.NETWORK_ADDRESS_RESPONSE ? ieeeAddress : networkAddress,
+                        target:
+                            responseClusterId === Zdo.ClusterId.NETWORK_ADDRESS_RESPONSE || responseClusterId === Zdo.ClusterId.LEAVE_RESPONSE
+                                ? ieeeAddress
+                                : networkAddress,
                     });
                 }
             }
@@ -282,9 +283,6 @@ class ZiGateAdapter extends Adapter {
                 const result = await waiter.start().promise;
 
                 return result.zdo as ZdoTypes.RequestToResponseMap[K];
-            } else if (clusterId === Zdo.ClusterId.LEAVE_REQUEST) {
-                // mock missing response (see above)
-                return [Zdo.Status.SUCCESS, undefined];
             }
         }, networkAddress);
     }
