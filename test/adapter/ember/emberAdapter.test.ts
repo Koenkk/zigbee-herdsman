627ea9b977a09e785026ab5b53a2b5cdffe5a3d1
Nerivec <62446222+Nerivec@users.noreply.github.com>
2024-12-17 17:05:07 +0100

    fix

--- test/adapter/ember/emberAdapter.test.ts
+++ test/adapter/ember/emberAdapter.test.ts
@@ -74,7 +74,7 @@ function reverseApsFrame(apsFrame: EmberApsFrame): EmberApsFrame {
 }
 
 function flushPromises(): Promise<void> {
-    return new Promise(jest.requireActual('timers').setImmediate);
+    return new Promise(jest.requireActual('node:timers').setImmediate);
 }
 
 const TEMP_PATH = path.resolve('temp');
