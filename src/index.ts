74fc5f0f5830ff0fad8df0b89c30bc30ad8b6697
Nerivec <62446222+Nerivec@users.noreply.github.com>
2024-12-17 20:47:17 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export {Controller} from './controller/controller';
-export type * as ControllerEvents from './controller/events';
-export type * as ControllerTypes from './controller/tstype';
+export type * as Events from './controller/events';
+export type * as Types from './controller/tstype';
+export type * as Models from './controller/model';
+export type * as AdapterEvents from './adapter/events';
 export type * as AdapterTypes from './adapter/tstype';
 export * as Zcl from './zspec/zcl';
 export * as Zdo from './zspec/zdo';
