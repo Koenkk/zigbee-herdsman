6ac8a9f3a26cb6cc6aab1747a53f1d40d09eaf90
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-09-15 01:37:21 +0200

    fix: add missing pulseWidthModulation cluster

--- src/zspec/zcl/definition/tstype.ts
+++ src/zspec/zcl/definition/tstype.ts
@@ -306,6 +306,7 @@ export type ClusterName =
     | "genMultistateValue"
     | "genCommissioning"
     | "genOta"
+    | "pulseWidthModulation"
     | "genPollCtrl"
     | "greenPower"
     | "mobileDeviceCfg"
