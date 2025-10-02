bf913c7ee01d04b5f133fde0e201b8a9347b0bee
David Dreschner <github-2017@dreschner.net>
2025-10-02 02:10:52 +0200

    fix: Move time-related code to singleton timeService

--- src/controller/model/device.ts
+++ src/controller/model/device.ts
@@ -1,9 +1,9 @@
 import assert from "node:assert";
-import {tzScan} from "@date-fns/tz";
 import type {Events as AdapterEvents} from "../../adapter";
 import type {LQINeighbor, RoutingTableEntry} from "../../adapter/tstype";
 import {wait} from "../../utils";
 import {logger} from "../../utils/logger";
+import * as timeService from "../../utils/timeService";
 import * as ZSpec from "../../zspec";
 import {BroadcastAddress} from "../../zspec/enums";
 import type {Eui64} from "../../zspec/tstypes";
@@ -346,100 +346,16 @@ export class Device extends Entity<ControllerEventMap> {
 
         // Response to read requests
         if (frame.header.isGlobal && frame.isCommand("read") && !this._customReadResponse?.(frame, endpoint)) {
-            const currentTime = Date.now();
-            const currentDate = new Date(currentTime);
-            const currentYear = currentDate.getUTCFullYear();
-
-            // Default values considering the timezone has no DST
-            let timeZoneDifferenceToUtc = currentDate.getTimezoneOffset() !== 0 ? currentDate.getTimezoneOffset() * -1 * 60 : 0;
-            let dstStart = 0xffffffff;
-            let dstEnd = 0xffffffff;
-            let dstChange = 0;
-            const standardTime = currentTime + timeZoneDifferenceToUtc * 1000;
-            let localTime = standardTime;
-
-            const localTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-            const dstChangesThisYear = tzScan(localTimeZone, {
-                start: new Date(currentYear, 1),
-                end: new Date(currentYear, 12),
-            });
-
-            // Countries may leave/introduce DST, which isn't supported here.
-            // Therefore, we always have to check that the returned number
-            // of changes is exactly 2.
-            const hasRegularDst = dstChangesThisYear.length === 2;
-            if (hasRegularDst) {
-                const firstDstChangeOfTheYear = dstChangesThisYear[0];
-
-                const isNorthernHemisphere = firstDstChangeOfTheYear.change > 0;
-                if (isNorthernHemisphere) {
-                    timeZoneDifferenceToUtc = dstChangesThisYear[1].offset * 60;
-                    dstStart = dstChangesThisYear[0].date.getTime();
-                    dstEnd = dstChangesThisYear[1].date.getTime();
-                    dstChange = dstChangesThisYear[0].change * 60;
-                } else {
-                    const dstStartIsInPreviousYear = localTime < dstChangesThisYear[0].date.getTime();
-                    if (dstStartIsInPreviousYear) {
-                        const dstChangesLastYear = tzScan(localTimeZone, {
-                            start: new Date(currentYear - 1, 1),
-                            end: new Date(currentYear - 1, 12),
-                        });
-
-                        const hadRegularDstLastYear = dstChangesLastYear.length === 2;
-                        if (hadRegularDstLastYear) {
-                            timeZoneDifferenceToUtc = dstChangesThisYear[0].offset * 60;
-                            dstStart = dstChangesLastYear[1].date.getTime();
-                            dstEnd = dstChangesThisYear[0].date.getTime();
-                            dstChange = dstChangesLastYear[1].change * 60;
-                        }
-                    } else {
-                        const dstChangesNextYear = tzScan(localTimeZone, {
-                            start: new Date(currentYear + 1, 1),
-                            end: new Date(currentYear + 1, 12),
-                        });
-
-                        const hasRegularDstNextYear = dstChangesThisYear.length === 2;
-                        if (hasRegularDstNextYear) {
-                            timeZoneDifferenceToUtc = dstChangesNextYear[0].offset * 60;
-                            dstStart = dstChangesThisYear[1].date.getTime();
-                            dstEnd = dstChangesNextYear[0].date.getTime();
-                            dstChange = dstChangesThisYear[1].change * 60;
-                        }
-                    }
-                }
-
-                const dstActive = localTime > dstStart && localTime < dstEnd;
-                if (dstActive) {
-                    localTime = standardTime + dstChange * 1000;
-                }
-            }
-
-            function toZigbeeUtcTime(timestamp: number) {
-                if (timestamp === 0xffffffff) {
-                    return timestamp;
-                }
-
-                const januaryFirst2000 = new Date("January 01, 2000 00:00:00 UTC+00:00").getTime();
-                return Math.round((timestamp - januaryFirst2000) / 1000);
-            }
-
             const attributes: {[s: string]: KeyValue} = {
                 ...endpoint.clusters,
-                genTime: {
-                    attributes: {
-                        time: toZigbeeUtcTime(currentTime),
-                        timeStatus: 3, // Time-master + synchronised
-                        timeZone: timeZoneDifferenceToUtc,
-                        dstStart: toZigbeeUtcTime(dstStart),
-                        dstEnd: toZigbeeUtcTime(dstEnd),
-                        dstShift: dstChange,
-                        standardTime: toZigbeeUtcTime(standardTime),
-                        localTime: toZigbeeUtcTime(localTime),
-                        lastSetTime: toZigbeeUtcTime(currentTime),
-                        validUntilTime: toZigbeeUtcTime(currentTime) + 24 * 60 * 60, // valid for 24 hours
-                    },
-                },
             };
+
+            const isTimeReadRequest = dataPayload.clusterID === Zcl.Clusters.genTime.ID;
+            if (isTimeReadRequest) {
+                attributes.genTime = {
+                    attributes: timeService.getTimeCluster(),
+                };
+            }
 
             if (frame.cluster.name in attributes) {
                 const response: KeyValue = {};
--- test/controller.test.ts
+++ test/controller.test.ts
@@ -13,6 +13,7 @@ import {InterviewState} from "../src/controller/model/device";
 import type * as Models from "../src/models";
 import * as Utils from "../src/utils";
 import {setLogger} from "../src/utils/logger";
+import * as timeService from "../src/utils/timeService";
 import * as ZSpec from "../src/zspec";
 import {BroadcastAddress} from "../src/zspec/enums";
 import * as Zcl from "../src/zspec/zcl";
@@ -2782,6 +2783,7 @@ describe("Controller", () => {
         const device = controller.getDeviceByIeeeAddr("0x129")!;
         expect(device.skipDefaultResponse).toBeFalsy();
         device.skipDefaultResponse = true;
+
         await mockAdapterEvents.zclPayload({
             wasBroadcast: false,
             address: 129,
@@ -2875,33 +2877,6 @@ describe("Controller", () => {
 
     it.each([
         {
-            testCase: "Timezone without DST",
-            timeZone: "Atlantic/Reykjavik",
-            localTime: "Wed Oct 01 2025 17:06:08 GMT+0000 (Greenwich Mean Time)",
-            expectedTime: 812653568,
-            expectedTimeZone: 0,
-            expectedDstStart: 0xffffffff,
-            expectedDstEnd: 0xffffffff,
-            expectedDstShift: 0,
-            expectedStandardTime: 812653568,
-            expectedLocalTime: 812653568,
-        },
-        {
-            testCase: "Northern Hemisphere, timezone with DST, DST active",
-            timeZone: "Europe/Berlin",
-            localTime: "Wed Oct 01 2025 18:49:34 GMT+0200 (Central European Summer Time)",
-            expectedTime: 812652574,
-            expectedTimeZone: 3600,
-            expectedDstStart: 796611600,
-            expectedDstEnd: 814755600,
-            expectedDstShift: 3600,
-            expectedStandardTime: 812659774,
-            expectedLocalTime: 812663374,
-        },
-        {
-            testCase: "Northern Hemisphere, timezone with DST, DST inactive",
-            timeZone: "Europe/Berlin",
-            localTime: "Mon Mar 02 2026 19:04:12 GMT+0100 (Central European Standard Time)",
             expectedTime: 825789852,
             expectedTimeZone: 3600,
             expectedDstStart: 828061200,
@@ -2910,47 +2885,9 @@ describe("Controller", () => {
             expectedStandardTime: 825793452,
             expectedLocalTime: 825793452,
         },
-        {
-            testCase: "Southern Hemisphere, timezone with DST, DST inactive",
-            timeZone: "Australia/Sydney",
-            localTime: "Thu Oct 02 2025 03:03:25 GMT+1000 (Australian Eastern Standard Time)",
-            expectedTime: 812653405,
-            expectedTimeZone: 36000,
-            expectedDstStart: 812908800,
-            expectedDstEnd: 828633600,
-            expectedDstShift: 3600,
-            expectedStandardTime: 812689405,
-            expectedLocalTime: 812689405,
-        },
-        {
-            testCase: "Southern Hemisphere, timezone with DST, DST active with start in current year",
-            timeZone: "Australia/Sydney",
-            localTime: "Mon Nov 03 2025 03:07:49 GMT+1100 (Australian Eastern Daylight Time)",
-            expectedTime: 815414869,
-            expectedTimeZone: 36000,
-            expectedDstStart: 812908800,
-            expectedDstEnd: 828633600,
-            expectedDstShift: 3600,
-            expectedStandardTime: 815454469,
-            expectedLocalTime: 815458069,
-        },
-        {
-            testCase: "Southern Hemisphere, timezone with DST, DST active with start in previous year",
-            timeZone: "Australia/Sydney",
-            localTime: "Mon Jan 19 2026 03:08:11 GMT+1100 (Australian Eastern Daylight Time)",
-            expectedTime: 822067691,
-            expectedTimeZone: 36000,
-            expectedDstStart: 812908800,
-            expectedDstEnd: 828633600,
-            expectedDstShift: 3600,
-            expectedStandardTime: 822107291,
-            expectedLocalTime: 822110891,
-        },
     ])(
-        "Respond to genTime read ($testCase)",
+        "Respond to genTime read",
         async ({
-            timeZone,
-            localTime,
             expectedTime,
             expectedTimeZone,
             expectedDstStart,
@@ -2959,8 +2896,20 @@ describe("Controller", () => {
             expectedStandardTime,
             expectedLocalTime,
         }) => {
-            vi.setSystemTime(new Date(localTime));
-            vi.stubEnv("TZ", timeZone);
+            // Mock the timeService, as we don't want to test that
+            vi.mock("../src/utils/timeService");
+            vi.mocked(timeService.getTimeCluster).mockReturnValue({
+                time: expectedTime,
+                timeStatus: 3,
+                timeZone: expectedTimeZone,
+                dstStart: expectedDstStart,
+                dstEnd: expectedDstEnd,
+                dstShift: expectedDstShift,
+                standardTime: expectedStandardTime,
+                localTime: expectedLocalTime,
+                lastSetTime: expectedTime,
+                validUntilTime: expectedTime + 24 * 60 * 60,
+            });
 
             const frame = Zcl.Frame.create(
                 0,
