25546806785992d09380cd339e88d4539296ec51
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-12-09 21:02:09 +0100

    fix: coverage ignore

--- src/controller/model/endpoint.ts
+++ src/controller/model/endpoint.ts
@@ -254,23 +254,20 @@ export class Endpoint extends ZigbeeEntity {
         }
 
         // Migrate cluster renames from https://github.com/Koenkk/zigbee-herdsman/pull/1503 @deprecated 3.0
-        /* v8 ignore next */
+        /* v8 ignore start */
         if (record.clusters.piRetailTunnel) {
             record.clusters.retailTunnel = record.clusters.piRetailTunnel;
             delete record.clusters.piRetailTunnel;
         }
-
-        /* v8 ignore next */
         if (record.clusters.tunneling) {
             record.clusters.seTunneling = record.clusters.tunneling;
             delete record.clusters.tunneling;
         }
-
-        /* v8 ignore next */
         if (record.clusters.haMeterIdentification) {
             record.clusters.seMeterIdentification = record.clusters.haMeterIdentification;
             delete record.clusters.haMeterIdentification;
         }
+        /* v8 ignore stop */
 
         return new Endpoint(
             record.epId,
