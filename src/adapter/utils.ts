8056438e799942dfe445520359867039d370398c
spike <spike@fedoraproject.org>
2025-12-20 04:24:55 +0100

    fix: allow literal (RFC2732) IPv6 addresses in TCP URI

--- src/adapter/utils.ts
+++ src/adapter/utils.ts
@@ -1,19 +1,20 @@
 import {existsSync, readFileSync} from "node:fs";
+import {urlToHttpOptions} from "node:url";
 import type * as Models from "../models";
 
 export function isTcpPath(path: string): boolean {
     // tcp path must be:
     // tcp://<host>:<port>
-    const regex = /^(?:tcp:\/\/)[\w.-]+[:][\d]+$/gm;
+    const regex = /^(?:tcp:\/\/)\S+?[:]\d+$/gm;
     return regex.test(path);
 }
 
 export function parseTcpPath(path: string): {host: string; port: number} {
     // built-in extra validation
-    const info = new URL(path);
+    const info = urlToHttpOptions(new URL(path));
 
     return {
-        host: info.hostname,
+        host: String(info.hostname),
         port: Number(info.port),
     };
 }
