0f8646ae9ff57dcb3294bd9eadc7cf6c757d0654
svhelge <79386304+svhelge@users.noreply.github.com>
2026-02-26 22:25:35 +0100

    Update tstype.ts

--- src/zspec/zcl/definition/tstype.ts
+++ src/zspec/zcl/definition/tstype.ts
@@ -428,7 +428,6 @@ export type ClusterName =
     | "manuSpecificCentraliteHumidity"
     | "manuSpecificSmartThingsArrivalSensor"
     | "manuSpecificSamsungAccelerometer"
-    | "tradfriButton"
     | "sprutVoc"
     | "sprutNoise"
     | "sprutIrBlaster"
