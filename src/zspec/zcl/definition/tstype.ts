6a3aa5f2aec57235e49abb97b0a57564fab15731
svhelge <79386304+svhelge@users.noreply.github.com>
2026-03-07 22:46:19 +0100

    Update tstype.ts

--- src/zspec/zcl/definition/tstype.ts
+++ src/zspec/zcl/definition/tstype.ts
@@ -421,7 +421,6 @@ export type ClusterName =
     | "manuSpecificLumi"
     | "manuSpecificTuya2"
     | "manuSpecificTuya3"
-    | "manuSpecificSmartThingsArrivalSensor"
     | "manuSpecificSiglisZigfred"
     | "owonClearMetering"
     | "zosungIRTransmit"
